77445db90d8686d9484f36e922b32d8768c7183f
atri.alfa <atri.alfa@maucash.id>
2024-02-14 22:16:21 +0700

    initiliaze

--- src/main/java/atri/smilee/report/ReportApplication.java
+++ src/main/java/atri/smilee/report/ReportApplication.java
@@ -3,11 +3,17 @@ package atri.smilee.report;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ReportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReportApplication.class, args);
+		SpringApplication app = new SpringApplication(ReportApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "9090"));
+		app.run(args);
+
 	}
 
 }
